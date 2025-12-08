9e79a6a884131ce5c3264353e624fb693fea9a93
Hossein Moradi <ho3inmoradi1998@gmail.com>
2025-12-08 12:56:44 +0330

    implement location store file

--- src/store/window.js
+++ src/store/window.js
@@ -2,7 +2,6 @@ import { create } from 'zustand'
 import {immer} from "zustand/middleware/immer";
 import {INITIAL_Z_INDEX, WINDOW_CONFIG} from "@constants/index.js";
 
-
 const useWindowStore = create(immer((set) => ({
     windows: WINDOW_CONFIG,
     nextZIndex: INITIAL_Z_INDEX + 1,
